5c77824ed25599094713c73c2247bd05a45fb519
tiga26 <tigranhovhannisyan88@gmail.com>
2014-03-27 01:32:59 +0400

    ajax backend

--- js/ajax.js
+++ js/ajax.js
@@ -14,29 +14,53 @@ jQuery(".rec_status").on('click', function() {
 	var data = {
 		id: 1,
 	};
-	var data = {
+	var datas = {
 		action: 'assay',
         data: data,
         type: 'get'
 	};
 	// the_ajax_script.ajaxurl is a variable that will contain the url to the ajax processing file
- 	jQuery.post(the_ajax_script.ajaxurl, data, function(response) {
+ 	jQuery.post(the_ajax_script.ajaxurl, datas, function(response) {
 		alert(response);
  	});
  	return false;
 });
 
+// jQuery("#dates").on('click', function() {
+// 	var data = {
+// 		rrlation_id: 1,
+// 		value: 20,
+// 		symptom_id: 2
+// 	};
+// 	var datas = {
+// 		action: 'symptom',
+//         data: data,
+//         type: 'set'
+// 	};
+// 	// the_ajax_script.ajaxurl is a variable that will contain the url to the ajax processing file
+//  	jQuery.post(the_ajax_script.ajaxurl, datas, function(response) {
+// 		alert(response);
+//  	});
+//  	return false;
+// });
+
 jQuery("#dates").on('click', function() {
 	var data = {
-		id: 1,
+		assay_id: 1,
+		relation_id: 1,
+		unit_id: 2,
+		result: 20,
+		ref_low: 7,
+		ref_hi: 10,
+		remarks: 'fsdfsdfsdfsdf'
 	};
-	var data = {
-		action: 'symptom',
+	var datas = {
+		action: 'assay',
         data: data,
-        type: 'get'
+        type: 'set'
 	};
 	// the_ajax_script.ajaxurl is a variable that will contain the url to the ajax processing file
- 	jQuery.post(the_ajax_script.ajaxurl, data, function(response) {
+ 	jQuery.post(the_ajax_script.ajaxurl, datas, function(response) {
 		alert(response);
  	});
  	return false;
